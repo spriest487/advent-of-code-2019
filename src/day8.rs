5a7d3e5feb87906b798433ce75d108ceaa44186c
spriest487 <spriest487@googlemail.com>
2019-12-10 01:31:34 +0000

    Day 8 part two

--- src/day8.rs
+++ src/day8.rs
@@ -1,12 +1,16 @@
+const BLACK: u32 = 0;
+const WHITE: u32 = 1;
+const CLEAR: u32 = 2;
+
 fn read_img(data: &str) -> Vec<u32> {
     data.chars()
         .map(|c| c.to_digit(10).unwrap())
         .collect()
 }
 
-fn image_checksum(image: &[u32], w: usize, h: usize) -> usize {
+fn image_checksum(data: &[u32], w: usize, h: usize) -> usize {
     let layer_len = w * h;
-    let layer_index = image.chunks(layer_len)
+    let layer_index = data.chunks(layer_len)
         .enumerate()
         .min_by_key(|(_index, layer)| {
             layer.iter().filter(|col| **col == 0).count()
@@ -18,7 +22,7 @@ fn image_checksum(image: &[u32], w: usize, h: usize) -> usize {
 
     let layer_start = layer_len * layer_index;
 
-    let (ones, twos) = image[layer_start..layer_start + layer_len].iter()
+    let (ones, twos) = data[layer_start..layer_start + layer_len].iter()
         .fold((0, 0), |(ones, twos), color| {
             match *color {
                 1 => (ones + 1, twos),
@@ -32,10 +36,50 @@ fn image_checksum(image: &[u32], w: usize, h: usize) -> usize {
     ones * twos
 }
 
+fn decode_image(data: &[u32], w: usize, h: usize) -> Vec<u32> {
+    let layer_len = w * h;
+
+    let mut output = vec![CLEAR; layer_len];
+
+    for layer in data.chunks(layer_len).rev() {
+        for y in 0..h {
+            for x in 0..w {
+                let off = y * w + x;
+                let dest = output[off];
+                let src = layer[off];
+
+                output[off] = match (src, dest) {
+                    (CLEAR, _) => dest,
+                    (_, CLEAR) => src,
+                    (color, _) => src,
+                };
+            }
+        }
+    }
+
+    output
+}
+
 fn main() {
     let input = include_str!("day8.txt");
-    let input_colors = read_img(input);
+    const W: usize = 25;
+    const H: usize = 6;
 
-    let checksum = image_checksum(&input_colors, 25, 6);
+    let data = read_img(input);
+
+    let checksum = image_checksum(&data, W, H);
     println!("image has checksum value {}", checksum);
+
+    let image = decode_image(&data, W, H);
+    for y in 0..H {
+        for x in 0..W {
+            let off = y * W + x;
+            print!("{}", match image[off] {
+                WHITE => '#',
+                BLACK => '.',
+                _ => ' ',
+            })
+        }
+        println!();
+    }
 }
